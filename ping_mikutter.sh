86a369d0fb3789119c2ddc95a0aff2cdc98f56dd
moguno <moguno@0kn.sakura.ne.jp>
2015-08-30 09:34:08 +0900

    サンプルスクリプトを更新

--- ping_mikutter.sh
+++ ping_mikutter.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
-ping -t 1 -c 1 $1 > /dev/null 2>&1
+ping -t 3 -c 3 $1 > /dev/null 2>&1
 
-if [ "$?" != 0 ];then
+result=$?
+
+if [ "$result" != 0 ];then
 	echo "マスター大変！$1が死んでるよ！" | nc localhost 39390
 fi
